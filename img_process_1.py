e415d30cea3da1af5987e6ea758d7e9c8d7fe6cd
MadhuMPandurangi <madhumpandurangi@gmail.com>
2022-06-18 10:22:31 +0530

    update: Added binary and binary inverted image

--- img_process_1.py
+++ img_process_1.py
@@ -1,27 +1,27 @@
 import cv2
 
-# reading image
+# Load the image
 image = cv2.imread('./static/MVI_1421-00008.jpg')
 
 
+# Converting to binary and display
 
-# Converting to binary 
 ret, thresh4 = cv2.threshold(image, 125, 255, cv2.THRESH_BINARY)
 
+ret1, thresh = cv2.threshold(image, 120, 255,cv2.THRESH_BINARY_INV)
 
-
-# Displying original image
 cv2.imshow('original', cv2.resize(image, (0, 0), fx = 0.3, fy = 0.3))
 
-# Displying converted image
 cv2.imshow('1st image', cv2.resize(thresh4, (0, 0), fx = 0.3, fy = 0.3))
 
+cv2.imshow('2nd image', cv2.resize(thresh, (0, 0), fx = 0.3, fy = 0.3))
 
-
-# Wait(display image) till any key is pressed
 cv2.waitKey(0)
 
-
 cv2.destroyAllWindows()
+
+
+
+
 
 
